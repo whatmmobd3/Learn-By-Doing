daa7705ccc580e5ef7b6ef7ed090832886242431
David Nguyen <david.nguyen@terralogic.com>
2022-07-13 13:43:18 +0700

    repeat foreach

--- Learn By Doing/ContentView.swift
+++ Learn By Doing/ContentView.swift
@@ -2,7 +2,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        CardView()
+        ScrollView(.horizontal, showsIndicators: false){
+            HStack{
+                ForEach(0 ..< 6) { item in
+                    CardView()
+                }
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
